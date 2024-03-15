a22969a7fefb0637467f43ae3681aff2ac0039e9
Ilyas022 <ilya.shchetko@mail.ru>
2024-03-15 12:49:29 +0300

    refactor: Move global styles and theme provider to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
 import FilmCards from './components/FilmCards'
 import Footer from './components/Footer'
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary'
 import { StyledWrongText } from 'components/FilmCards/styled'
-import { GlobalStyles, StyledWrapper } from './global.styles'
-import { useTypedSelector } from 'hooks/useTypedSelector'
+import { StyledWrapper } from './global.styles'
 
 const App: React.FC = (): JSX.Element => {
-	const { theme } = useTypedSelector((state) => state.theme)
-
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyles />
-			<StyledWrapper>
-				<Header />
-				<main>
-					<ErrorBoundary
-						fallback={
-							<StyledWrongText>
-								There are no such videos or or the maximum number of api requests has been exceeded
-							</StyledWrongText>
-						}
-					>
-						<FilmCards />
-					</ErrorBoundary>
-				</main>
-				<Footer />
-			</StyledWrapper>
-		</ThemeProvider>
+		<StyledWrapper>
+			<Header />
+			<main>
+				<ErrorBoundary
+					fallback={
+						<StyledWrongText>
+							There are no such videos or or the maximum number of api requests has been exceeded
+						</StyledWrongText>
+					}
+				>
+					<FilmCards />
+				</ErrorBoundary>
+			</main>
+			<Footer />
+		</StyledWrapper>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './global.styles'
+import { useTypedSelector } from 'hooks/useTypedSelector'
+
+const RootComponent = () => {
+	const { theme } = useTypedSelector((state) => state.theme)
+
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<App />
+		</ThemeProvider>
+	)
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<RootComponent />
 		</Provider>
 	</React.StrictMode>
 )
